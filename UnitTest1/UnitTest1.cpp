2058ec34db734c943f4e620331447cb1cd8c3b1c
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-10-31 22:45:41 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 5.4/Lab 5.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double test;
+			test = S0(4);
+			Assert::AreEqual(test, 20.7358, 0.0001);
+
+			test = S1(4, 4);
+			Assert::AreEqual(test, 20.7358, 0.0001);
+
+			test = S2(4, 22);
+			Assert::AreEqual(test, 20.7358, 0.0001);
+
+			test = S3(4, 4, 0);
+			Assert::AreEqual(test, 20.7358, 0.0001);
+
+			test = S4(4, 22, 0);
+			Assert::AreEqual(test, 20.7358, 0.0001);
+		}
+	};
+}
